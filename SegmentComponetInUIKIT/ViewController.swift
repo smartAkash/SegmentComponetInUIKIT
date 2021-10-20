e826c9851ce9de85a1879110dca67beb433208af
Akash <akash.nara@smartsensesolutions.com>
2021-10-20 12:47:16 +0530

    Initial Commit

---
+++ SegmentComponetInUIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentComponetInUIKIT
+//
+//  Created by Akash on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
